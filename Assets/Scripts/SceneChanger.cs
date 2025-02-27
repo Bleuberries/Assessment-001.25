a52d0ce73436f7776439ad5513f6670cc7626af9
Mina <mylastnameiskumai@gmail.com>
2025-02-27 17:42:58 -0500

    Created SceneChanger.cs
    (All in 1Scene)

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; //changes scene
+using UnityEngine.SceneManagement; //Changes scene
 
 public class SceneChanger : MonoBehaviour
 {
